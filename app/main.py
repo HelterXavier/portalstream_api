a91a6f03bf0cbcc5d1bc89deb99f83dec0a67a43
Helter Xavier <helter.xavier@semeq.com>
2025-05-19 16:05:07 -0300

    CORS config

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.auth import router as auth_router
 from app.routes.usercorp import router as usercorp_router
 from app.routes.implantation import router as implantation_router
 
-
 app = FastAPI(title="Portal Steam API")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(usercorp_router)
